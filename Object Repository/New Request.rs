14fcdacd1b797dafa55a3789fa42900aaa97d712
Patric Bidiviu <bidiviu.patric@gmail.com>
2020-04-07 22:09:01 +0300

    New Request API

---
+++ Object Repository/New Request.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebServiceRequestEntity>
+   <description></description>
+   <name>New Request</name>
+   <tag></tag>
+   <elementGuidId>37f1d2b2-5f5d-496d-a0cc-e45dac0e0641</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <followRedirects>false</followRedirects>
+   <httpBody></httpBody>
+   <httpBodyContent></httpBodyContent>
+   <httpBodyType></httpBodyType>
+   <migratedVersion>5.4.1</migratedVersion>
+   <restRequestMethod>GET</restRequestMethod>
+   <restUrl>https://rapidapi.com/teams?utm_source=google&amp;utm_medium=cpc&amp;utm_campaign=2051169303_80056715881&amp;utm_term=%2Bapi%20%2Btest_b&amp;utm_content=&amp;gclid=EAIaIQobChMI6cbf-v_W6AIVS6WaCh1JcQJaEAAYASAAEgIiSfD_BwE</restUrl>
+   <serviceType>RESTful</serviceType>
+   <soapBody></soapBody>
+   <soapHeader></soapHeader>
+   <soapRequestMethod></soapRequestMethod>
+   <soapServiceFunction></soapServiceFunction>
+   <wsdlAddress></wsdlAddress>
+</WebServiceRequestEntity>
